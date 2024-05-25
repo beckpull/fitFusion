6de2a7146d4bb0222c81bfabbf14622972277dd4
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-24 23:02:27 -0600

    Adding components folder to the client side

--- client/App.js
+++ client/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{ uri: 'https://i.gifer.com/Ul9r.gif' }} // Can be a URL or a local file.
+        style={{ width: 400, height: 400}}
+      />
       <StatusBar style="auto" />
     </View>
   );
