93340ea590f3e8fecd1ff0c0713d92b8fed416dc
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-05-24 23:18:41 -0600

    Video Main page added

--- client/App.js
+++ client/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
+import { Video } from 'expo-av';
+import videoFile from './assets/videos/mainVideo.mp4'; // replace with your video file path
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: 'https://i.gifer.com/Ul9r.gif' }} // Can be a URL or a local file.
-        style={{ width: 400, height: 400}}
+      <Video
+        source={videoFile}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        resizeMode="cover"
+        shouldPlay
+        isLooping
+        style={{ width: '100%', height: '100%' }}
       />
       <StatusBar style="auto" />
     </View>
