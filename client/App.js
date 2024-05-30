23a9e1e00bf27b6c835d4a5c66e614b4946b6bf4
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-05-29 18:15:21 -0600

    Merged all conflicts before push.

--- client/App.js
+++ client/App.js
@@ -28,11 +28,11 @@ export default function App() {
     <ApolloProvider client={client}>
       <NavigationContainer>
         <Stack.Navigator>
-          {/* <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="LoginForm" component={LoginForm} />
-          <Stack.Screen name="SignUpForm" component={SignUpForm} /> */}
+          <Stack.Screen name="SignUpForm" component={SignUpForm} />
 
-          {/* JRH <> */}
+          {/* API Search Screens (From JRH) <> */}
           <Stack.Screen name="MainSearchScreen" component={MainSearchScreen} options={{ title: 'Search for a Workout' }} />
           <Stack.Screen name="SearchByNameScreen" component={SearchByNameScreen} options={{ title: 'Search by Name' }} />
           <Stack.Screen name="SearchByMuscleScreen" component={SearchByMuscleScreen} options={{ title: 'Search by Targeted Muscle' }} />
