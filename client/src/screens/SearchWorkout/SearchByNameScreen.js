a04c7b0154d506830fb1f7dada6c964bb2154b02
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-06-03 23:41:06 -0600

    Add button working for exercises

--- client/src/screens/SearchWorkout/SearchByNameScreen.js
+++ client/src/screens/SearchWorkout/SearchByNameScreen.js
@@ -11,116 +11,114 @@ import Workouts from '../../components/searchResults/ExerciseResults';
 
 
 export default function SearchByNameScreen() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [exercise, setExercise] = useState([]);
-  const [loading, setLoading] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [exercise, setexercise] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  const { currentWorkoutId } = useContext(WorkoutContext);
-  const [addWorkout, { error }] = useMutation(ADD_WORKOUT);
+    const { currentWorkoutId } = useContext(WorkoutContext);
+    const [addWorkout, { error }] = useMutation(ADD_WORKOUT);
 
-  const handleSearch = async (query) => {
-    setSearchQuery(query);
-    console.log('Searching for:', query);
+    const handleSearch = async (query) => {
+        setSearchQuery(query);
+        console.log('Searching for:', query);
 
-    // API Results for searchQuery
-    setLoading(true);
-    const url = `https://exercisedb.p.rapidapi.com/exercises/name/${searchQuery.toLowerCase()}`;
-    try {
-      const response = await axios.get(url, {
-        params: { limit: '50' },
-        headers: {
-          'X-RapidAPI-Key': '1bc24f2cf3msh0169303f2df7b55p108af6jsn0e1789b7d77c',
-          'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        },
-      });
+        // API Results for searchQuery
+        setLoading(true);
+        const url = `https://exercisedb.p.rapidapi.com/exercises/name/${searchQuery.toLowerCase()}`;
+        try {
+            const response = await axios.get(url, {
+                params: { limit: '50' },
+                headers: {
+                    'X-RapidAPI-Key': '1bc24f2cf3msh0169303f2df7b55p108af6jsn0e1789b7d77c',
+                    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+                },
+            });
 
-      setExercise(response.data);
-    } catch (error) {
-      // console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleAddWorkout = async (exercise) => {
-    console.log(exercise.instructions);
-    console.log(exercise.secondaryMuscles);
-    console.log(`The current ID is ${currentWorkoutId}`)
-    try {
-      const { data } = await addWorkout({
-        variables: {
-          workoutPlanId: currentWorkoutId,
-          workoutInput: {
-            name: exercise.name,
-            workoutId: Number(exercise.id),
-            bodyPart: exercise.bodyPart,
-            equipment: exercise.equipment,
-            gifUrl: exercise.gifUrl,
-            target: exercise.target,
-            instructions: exercise.instructions,
-            secondary: exercise.secondaryMuscles,
-          }
+            setexercise(response.data);
+        } catch (error) {
+            // console.error(error);
+        } finally {
+            setLoading(false);
         }
-      });
-      Alert.alert('Success', 'Exercise added to workout plan');
-    } catch (error) {
-      console.error(error);
-      Alert.alert('Error', 'Failed to add exercise to workout plan');
-    }
-  };
+    };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Search for an exercise name:</Text>
-      <View style={styles.search}>
-        <SearchBar
-          placeholder="Search..."
-          onChangeText={handleSearch}
-          value={searchQuery}
-          platform="default"
-        />
+    const handleAddWorkout = async (exercise) => {
+       console.log(`The current ID is ${currentWorkoutId}`)
+        try {
+            const { data } = await addWorkout({
+                variables: {
+                    workoutPlanId: currentWorkoutId,
+                    workoutInput: {
+                        name: exercise.name,
+                        workoutId: Number(exercise.id),
+                        bodyPart: exercise.bodyPart,
+                        equipment: exercise.equipment,
+                        gifUrl: exercise.gifUrl,
+                        target: exercise.target,
+                        instructions: exercise.instructions.join(', '),
+                        secondary: exercise.secondaryMuscles.join(', '), 
+                    }
+                }
+            });
+            Alert.alert('Success', 'Exercise added to workout plan');
+        } catch (error) {
+            console.error(error);
+            Alert.alert('Error', 'Failed to add exercise to workout plan');
+        }
+    };
 
-        {loading ? (
-          <Text style={styles.message}>Loading...</Text>
-        ) : (
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Search for an exercise name:</Text>
+            <View style={styles.search}>
+                <SearchBar
+                    placeholder="Search..."
+                    onChangeText={handleSearch}
+                    value={searchQuery}
+                    platform="default"
+                />
 
-          <ScrollView>
-            <View style={styles.container}>
-              <Workouts workouts={exercise} onAdd={handleAddWorkout} />
+                {loading ? (
+                    <Text style={styles.message}>Loading...</Text>
+                ) : (
+ 
+                    <ScrollView>
+                        <View style={styles.container}>
+                            <Workouts workouts={exercise} onAdd={handleAddWorkout}/>
+                        </View>
+                    </ScrollView>
+          
+                )}
             </View>
-          </ScrollView>
-
-        )}
-      </View>
-    </View>
-  );
+        </View>
+    );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'top',
-  },
-  title: {
-    lineHeight: 40,
-    color: 'black',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  search: {
-    width: '90%',
-    padding: 16,
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'top',
+    },
+    title: {
+        lineHeight: 40,
+        color: 'black',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    search: {
+        width: '90%',
+        padding: 16,
+        
+    },
 
-  },
-
-  message: {
-    marginTop: 20,
-    fontSize: 18,
-    color: 'green',
-  },
+    message: {
+        marginTop: 20,
+        fontSize: 18,
+        color: 'green',
+    },
 });
