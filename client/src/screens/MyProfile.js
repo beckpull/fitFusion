7516f4fe84be2fe8bec249463a5f723033ba0b44
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-06-05 17:24:28 -0600

    edited placeholder image to be selectedImage if the user has one

--- client/src/screens/MyProfile.js
+++ client/src/screens/MyProfile.js
@@ -36,12 +36,12 @@ export default function MyProfile() {
       setImage({ uri: `data:image/jpeg;base64,${data.me.profilePic.data}` })
     }
   }, [data]);
-  
+
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
-  
+
   const { me: { username, level, workoutPlans } } = data;
-  
+
   const levelTranslationKey = `level${level}`;
 
   const pickImageAsync = async () => {
@@ -66,32 +66,32 @@ export default function MyProfile() {
         }
       })
 
-      .then(response => {
-        console.log("profile picture updated");
-        setImage({uri: `data:${profilePicContentType};base64,${profilePicData}`})
-      })
-      .catch(error => {
-        console.error('Error updating picture:', error);
-        Alert.alert("Error updating profile picture, please try again");
-      })
+        .then(response => {
+          console.log("profile picture updated");
+          setImage({ uri: `data:${profilePicContentType};base64,${profilePicData}` })
+        })
+        .catch(error => {
+          console.error('Error updating picture:', error);
+          Alert.alert("Error updating profile picture, please try again");
+        })
     } else {
 
       Alert.alert('You did not select any image.');
     }
   };
-  
+
   const handleClick = () => {
     const url = 'https://open.spotify.com/playlist/1Tq5PyQCvmwFUW17fxcabR?si=20af5756d6e04664';
-    
+
     Linking.canOpenURL(url)
-    .then((supported) => {
-      if (supported) {
-        return Linking.openURL(url);
-      } else {
-        console.log(`Don't know how to open this URL: ${url}`);
-      }
-    })
-    .catch((err) => console.error('An error occurred', err));
+      .then((supported) => {
+        if (supported) {
+          return Linking.openURL(url);
+        } else {
+          console.log(`Don't know how to open this URL: ${url}`);
+        }
+      })
+      .catch((err) => console.error('An error occurred', err));
   };
   // console.log("This is Image: ", image)
   return (
@@ -100,11 +100,10 @@ export default function MyProfile() {
         <TouchableOpacity style={styles.card}>
           <View style={styles.imageWrapper}>
             {loading ? <Text>{i18n.t('Loading')}...</Text> : image && (
-              <Image
-              source={image}
-              style={{ width: 100, height: 100, borderRadius: 50 }}
-              // placeholderImageSource={PlaceholderImage}
-              // selectedImage={selectedImage}
+              <UserImage
+                username={username}
+                placeholderImageSource={PlaceholderImage}
+                selectedImage={image.uri}
               />
             )}
           </View>
